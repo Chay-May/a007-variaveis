92dba0d4b2809907f5fd54688df9a32928e01c23
Chay-May <chayenne.mayara@gmail.com>
2022-07-18 22:59:34 -0300

    respostas do exercicio 007

---
+++ exercicio1/script.js
@@ -0,0 +1,16 @@
+/*let nome = ""
+let idade =*/
+
+/*A resposta apareceu como "erro" porque não possui valores atribuídos*/
+
+let nome = "Sebastiana"
+let idade = 35
+console.log (nome)
+console.log (idade)
+console.log (typeof nome)
+console.log (typeof idade)
+
+/*A resposta apareceu como "String" porque apesar de se referirem a valores (escrita e número) 
+diferentes, ambas são lidas como texto.)*/
+
+console.log ("Olá,", nome, "você tem", idade, "anos")
---
+++ exercicio2/script.js
@@ -0,0 +1,26 @@
+/*const sorvete = prompt("Você gosta de sorvete?")
+const oculos = prompt("Você usa óculos?")
+const anime = prompt("Você gosta de animes?")
+
+console.log(sorvete)
+console.log(anime)
+console.log(oculos)*/
+
+const sorveteperg = "Você gosta de sorvete?"
+const sorveteresp = prompt(sorveteperg)
+console.log(sorveteperg)
+console.log(sorveteresp)
+
+const oculosperg = "Você usa óculos?"
+const oculosresp =prompt(oculosperg)
+console.log(oculosperg)
+console.log(oculosresp)
+
+const animeperg = "Você gosta de animes?"
+const animeresp = prompt(animeperg)
+console.log(animeperg)
+console.log(animeresp)
+
+
+
+
